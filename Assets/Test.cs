a643735e3bb5d5573c0102648e5e37662252d36a
ヨコタタクヤ <yktk2346@gmail.com>
2017-02-01 21:53:33 +0900

    再送　ScriptTestプロジェクト　スクリプトの不具合
    確認をよろしくお願いします。

--- Assets/Test.cs
+++ Assets/Test.cs
@@ -12,6 +12,11 @@ public class Test: MonoBehaviour {
         //配列の各要素を順番に表示する
         for (int i = 0; i < array.Length; i++) {
             Debug.Log(array[i]);
+
+            //配列の各要素を逆順に表示する
+            for (int h = 4; h > 0; h--) {
+                Debug.Log(array[h]);
+            }
         }
 	}
 	
