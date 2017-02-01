5f3e5ed1e7bef722a49ab7d1b39cd2c916820ed7
ヨコタタクヤ <yktk2346@gmail.com>
2017-02-01 20:25:02 +0900

    ScriptTestプロジェクト　スクリプトの不具合
    確認をよろしくお願いします。

---
+++ Assets/Test.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Test: MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+        //要素数が5の、int型の配列arrayを宣言し好きな値で初期化する
+        int[] array = { 30, 20, 50, 10, 80 };
+
+        //配列の各要素を順番に表示する
+        for (int i = 0; i < array.Length; i++) {
+            Debug.Log(array[i]);
+        }
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
